23ce67c17614059e6a8bb43857e23062a372624f
Katlyn Boches <katlynboches@gmail.com>
2021-11-04 14:17:09 -0600

    updated Markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,12 +3,12 @@ function generateMarkdown(data) {
   console.log(data);
     return`
     
-## ${data.title}
+## **${data.title}**
 
-## Description
+## **Description**
 ${data.description}
 
-## Table of Contents
+## **Table of Contents**
 1. [Installation](#installation)
 2. [Usage](#usage)
 3. [License](#license)
@@ -16,25 +16,25 @@ ${data.description}
 5. [Tests](#tests)
 6. [Questions](#questions)
 
-## Installation
-To install the necessary dependencies, run the following command:
+## **Installation**
+![icons8-support-96](https://user-images.githubusercontent.com/86095070/140413027-0b255eb6-d5b2-48b8-83e8-4de45a1f15b3.png) To install the necessary dependencies, run the following command:
 ${data.installation}
 
-## Usage
-${data.usage}
+## **Usage**
+![icons8-services-96](https://user-images.githubusercontent.com/86095070/140413028-28f6d599-b123-41e3-8ca5-25fe4b2b7805.png) ${data.usage}
 
-## License 
+## **License** 
 This project is licensed under the ${data.license} license.
 
-## Contributing
-${data.contribution}
+## **Contributing**
+![icons8-search-96](https://user-images.githubusercontent.com/86095070/140413030-e0a58b78-e25e-47c4-ad69-028482a3dd90.png) ${data.contribution}
 
-## Tests
+## **Tests**
 To run tests, run the following command:
 ${data.tests}
 
-## Questions
-If you have any questions about this repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](https://www.github.com/${data.username}).
+## **Questions**
+If you have any questions about this repo, open an issue or contact me directly at ![icons8-gmail-96](https://user-images.githubusercontent.com/86095070/140413032-8279b840-208c-4826-b031-3524102d39e6.png) [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](https://www.github.com/${data.username}).
   `;
 }
 
